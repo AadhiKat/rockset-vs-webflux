a94a659511d6243f4baedb01831807952bce0497
AadhiKat <aadhithya.hari@meesho.com>
2022-04-12 11:25:19 +0530

    Rest Controller - converting it to Mono

--- rest-service/src/main/java/com/aadhikat/rest/controller/RestController.java
+++ rest-service/src/main/java/com/aadhikat/rest/controller/RestController.java
@@ -3,13 +3,14 @@ package com.aadhikat.rest.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import reactor.core.publisher.Mono;
 
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("rest-service")
 public class RestController {
 
     @GetMapping("rest/square/{input}")
-    public Integer findSquare(@PathVariable int input) {
-        return input * input;
+    public Mono<Integer> findSquare(@PathVariable int input) {
+        return Mono.just(input * input);
     }
 }
